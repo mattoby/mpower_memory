6076bd100da27bd7a406577005fed17474e29afe
mattoby <mattoby@gmail.com>
2016-01-21 17:20:53 -0500

    Added an anova, and have the logistic regression working now.

--- MemoryAnalysis.py
+++ MemoryAnalysis.py
@@ -1,3 +1,6 @@
+# this file is where i'm playing with new methods..
+
+
 import synapseclient
 from synapseclient import Project, Folder, File
 import pandas as pd
@@ -8,85 +11,15 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 import os
-%matplotlib inline
+import memorytools as mt
 # from pandas import DataFrame, Series
-
-
 %pylab # if running in ipython... (so i can see plots)
 
-
-# log in
-def login_synapse(username, password):
-    syn = synapseclient.Synapse()
-    syn.login(username, password) # need to change this, security.
-    return syn
-
-# get the memory table up and running:
-def load_memory_table_from_synapse(syn):
-    memorysyn = syn.tableQuery("select * from %s" % 'syn5511434')
-    memory = memorysyn.asDataFrame()
-
-    # rename columns:
-    memory.columns = [u'recordId', u'healthCode', u'createdOn',
-        u'appVersion', u'phoneInfo', u'game_score',
-        u'game_numGames', u'game_numFails', u'game_startDate',
-        u'game_endDate', u'game_records', u'medTimepoint']
-
-    # remove rows with null in the game_records column:
-    memory = memory.dropna(subset=["game_records"])
-
-    # Convert 'game_records' into a text key that matches filePaths:
-    for rec in memory.game_records:
-        assert (len(str(rec)) == 9), "records will not be parsed right since they are not the correct length - check %s" % rec
-    memory['game_records_txt'] = memory.game_records.apply(lambda r: str(r)[:7])
-
-    # finish:
-    return memory, memorysyn
-
-# get the demographics table up and running:
-def load_demographics_table_from_synapse(syn):
-    demosyn = syn.tableQuery("select * from %s" % 'syn5511429')
-    demographics = demosyn.asDataFrame()
-
-    # rename columns:
-    demographics.columns = ([u'recordId', u'healthCode', u'createdOn', u'appVersion', u'phoneInfo',
-       u'age', u'isCaretaker', u'brainStim', u'diagYear',
-       u'education', u'employment', u'gender', u'healthHistory',
-       u'healthcareProvider', u'homeUsage', u'lastSmoked', u'maritalStatus',
-       u'medicalUsage', u'medicalUsageYesterday', u'medicationStartYear',
-       u'onsetYear', u'packsPerDay', u'pastParticipation', u'phoneUsage',
-       u'professionalDiagnosis', u'race', u'smartphone', u'smoked',
-       u'surgery', u'videoUsage', u'yearsSmoking'])
-
-    # finish:
-    return demographics, demosyn
-
-
-# get the json files (slow, only load from scratch once):
-def load_memory_game_results_from_synapse(syn, memorysyn, fromScratch = False):
-    if fromScratch:
-        filePaths = syn.downloadTableColumns(memorysyn, u'MemoryGameResults.json.MemoryGameGameRecords')
-        pickle.dump( filePaths, open( "filePaths_for_memory.p", "wb" ) )
-    else:
-        filePaths = pickle.load( open( "filePaths_for_memory.p", "rb" ) )
-    return filePaths
-
-
-# load in the json data for a memory test:
-def load_memory_results_json(filePaths, game_record_txt):
-#    with open(filePaths[u'5732386']) as data_file:
-    with open(filePaths[game_record_txt]) as data_file:
-        game_record = json.load(data_file)
-    return game_record
-
-
-
-
 ## load up data:
-syn = login_synapse(os.environ['SYNAPSE_USER'], os.environ['SYNAPSE_PASS'])
-memory, memorysyn = load_memory_table_from_synapse(syn)
-filePaths = load_memory_game_results_from_synapse(syn, memorysyn)
-demographics, demosyn = load_demographics_table_from_synapse(syn)
+syn = mt.login_synapse(os.environ['SYNAPSE_USER'], os.environ['SYNAPSE_PASS'])
+memory, memorysyn = mt.load_memory_table_from_synapse(syn)
+filePaths = mt.load_memory_game_results_from_synapse(syn, memorysyn)
+demographics, demosyn = mt.load_demographics_table_from_synapse(syn)
 
 ## join dataframes:
 data = pd.merge(left=memory, right=demographics, how='inner', left_on='healthCode', right_on='healthCode')
@@ -133,9 +66,6 @@ line = plt.Polygon(points, closed=None, fill=None, edgecolor='r')
 
 
 filePaths[df.ix[0, 'deviceMotion_walking_outbound.json.items’)]
-
-
-
 filePaths[df.ix[0, 'deviceMotion_walking_outbound.json.items’)]
 
 
